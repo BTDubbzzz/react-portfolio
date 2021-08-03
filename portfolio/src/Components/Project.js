3afb95456d5fdf33d0376d7e9ee9064f2c70f9fa
Blake <btdubbzzz@gmail.com>
2021-08-03 18:17:15 -0500

    refactored projects into functional component

--- portfolio/src/Components/Project.js
+++ portfolio/src/Components/Project.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Project extends Component {
-	render() {
-		const { title, category, image, url } = this.props.data;
-		const projectImage = 'images/portfolio/' + image;
+function Project(props) {
+	const { title, category, image, url } = props.data;
+	const projectImage = 'images/portfolio/' + image;
 
-		return (
-			<div key={title} className="columns portfolio-item">
-				<div className="item-wrap">
-					<a href={url} target="_blank" rel="noopener noreferrer" title={title}>
-						<img alt={title} src={projectImage} />
-						<div className="overlay">
-							<div className="portfolio-item-meta">
-								<h5>{title}</h5>
-								<p>{category}</p>
-							</div>
+	return (
+		<div key={title} className="columns portfolio-item">
+			<div className="item-wrap">
+				<a href={url} target="_blank" rel="noopener noreferrer" title={title}>
+					<img alt={title} src={projectImage} />
+					<div className="overlay">
+						<div className="portfolio-item-meta">
+							<h5>{title}</h5>
+							<p>{category}</p>
 						</div>
-						<div className="link-icon">
-							<i className="fa fa-link"></i>
-						</div>
-					</a>
-				</div>
+					</div>
+					<div className="link-icon">
+						<i className="fa fa-link"></i>
+					</div>
+				</a>
 			</div>
-		);
-	}
+		</div>
+	);
 }
 
 export default Project;
