b8aeff5aded74e9a6b1cb0e3b02266891b3554b0
Blake <btdubbzzz@gmail.com>
2021-08-03 17:01:46 -0500

    Updates

--- js/init.js
+++ js/init.js
@@ -21,7 +21,7 @@ jQuery(document).ready(function ($) {
 ------------------------------------------------------ */
 
 	$('.smoothscroll').on('click', function (e) {
-		e.preventDefault();
+		// e.preventDefault();
 
 		var target = this.hash,
 			$target = $(target);
